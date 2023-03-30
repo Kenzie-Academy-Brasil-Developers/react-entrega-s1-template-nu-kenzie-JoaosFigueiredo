9b802b79cbd465e5de4ff476ab9b6fe22f418294
JoãoFigueiredo <jp5398@hotmail.com>
2023-03-30 16:06:58 -0300

    text/form

--- my-react-app/src/components/TransactionForm/index.jsx
+++ my-react-app/src/components/TransactionForm/index.jsx
@@ -17,7 +17,7 @@ export const TransactionForm = ({ addNewTransaction }) => {
             <form onSubmit={submitTransaction}>
                 <label htmlFor="description">Descrição</label>
                 <input type="text" name="description" id="description" required placeholder="Digite aqui sua descrição" value={description} onChange={(event) => setDescription(event.target.value)}/>
-                <span>Ex: compra de roupas</span>
+                <span className="bodyText" >Ex: compra de roupas</span>
                 <label htmlFor="transactionValue">Valor (R$)</label>
                 <input type="number" name="transactionValue" className="valueInput" id="transactionValue" placeholder="R$ 1" required step="0.01" min="0.01" value={value} onChange={(event) => setValue(event.target.value)}/>
                 <label htmlFor="transactionType">Tipo de valor</label>
